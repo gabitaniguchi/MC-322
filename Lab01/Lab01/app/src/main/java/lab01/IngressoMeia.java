014699ffea3c568ea3cbc665698c872940210023
Gabriela Taniguchi <110126586+gabitaniguchi@users.noreply.github.com>
2025-03-27 12:17:52 -0300

    Update IngressoMeia.java

--- Lab01/Lab01/app/src/main/java/lab01/IngressoMeia.java
+++ Lab01/Lab01/app/src/main/java/lab01/IngressoMeia.java
@@ -1,5 +1,25 @@
 package lab01;
 
 public class IngressoMeia extends Ingresso{
-    //IMPLEMENTAR
+   
+    /**
+     * Construtor da classe IngressoMeia
+     * @param evento o evento associado ao IngressoMeia
+     */
+    public IngressoMeia(Evento evento){
+        super(evento);
+    }
+
+    /**
+     * Retorna o preço do Ingresso
+     * @return o preço do Ingresso
+     */
+
+    @Override
+    public double getPreco() {
+        // Ingresso meia recebe metade do valor do ingresso do evento
+        return this.getEvento().getPrecoIngresso()/2;
+    }
+
+
 }
