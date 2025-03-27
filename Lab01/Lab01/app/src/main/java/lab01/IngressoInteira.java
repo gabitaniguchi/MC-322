f48457f6f06e67bf18f514c634b90eb6c48ffdf6
Gabriela Taniguchi <110126586+gabitaniguchi@users.noreply.github.com>
2025-03-27 12:17:36 -0300

    Update IngressoInteira.java

--- Lab01/Lab01/app/src/main/java/lab01/IngressoInteira.java
+++ Lab01/Lab01/app/src/main/java/lab01/IngressoInteira.java
@@ -1,5 +1,24 @@
 package lab01;
 
 public class IngressoInteira extends Ingresso{
-    //IMPLEMENTAR
+
+    /**
+     * Construtor da classe IngressoInteira
+     * @param evento o evento associado ao IngressoInteira
+     */
+    public IngressoInteira(Evento evento){
+        super(evento);
+    }
+
+    /**
+     * Retorna o preço do Ingresso
+     * @return o preço do Ingresso
+     */
+
+    @Override
+    public double getPreco() {
+        // Ingresso inteira recebe o valor integral do ingresso do evento
+        return this.getEvento().getPrecoIngresso();
+    }
+
 }
