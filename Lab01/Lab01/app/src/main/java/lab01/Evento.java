5e0dd340ae8b0e03df073b4804643ca745064b51
Gabriela Taniguchi <110126586+gabitaniguchi@users.noreply.github.com>
2025-03-27 12:15:17 -0300

    Update Evento.java

--- Lab01/Lab01/app/src/main/java/lab01/Evento.java
+++ Lab01/Lab01/app/src/main/java/lab01/Evento.java
@@ -6,16 +6,19 @@
 
 package lab01;
 
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * Contém a estrutura de implementação de um Evento.
  * 
- * @author Gabriel Leite - 216180
- * @author Caio Rhoden - 214129
+ * @author Gabriela Taniguchi - 281773
  */
 public abstract class Evento {
     private String nome;
     private Local local;
     private double precoIngresso;
+    private List<Ingresso> ingressosVendidos;
 
     /**
      * Construtor da classe Evento
@@ -26,6 +29,7 @@ public abstract class Evento {
         this.nome = nome;
         this.local = local;
         this.precoIngresso = precoIngresso;
+        this.ingressosVendidos = new ArrayList<>();
     }
 
     /**
@@ -37,11 +41,27 @@ public abstract class Evento {
     }
 
     /**
-     * Altera o nome do Evento para `nome` 
+     * Altera o nome do Evento para 'nome' 
      * @param nome o novo nome do Evento
      */
     public void setNome(String nome){
         this.nome = nome;
+    }
+
+    /**
+     * Retorna o local do Evento
+     * @return o local do Evento
+     */
+    public Local getLocal(){
+        return local;
+    }
+
+    /**
+     * Altera o local do Evento para 'local' 
+     * @param local o novo local do Evento
+     */
+    public void setLocal(Local local){
+        this.local = local;
     }
 
     /**
@@ -58,5 +78,36 @@ public abstract class Evento {
      */
     public void setPrecoIngresso(double precoIngresso){
         this.precoIngresso = precoIngresso;
+    }
+
+    /**
+     * Adiciona o ingresso vendido ao usuário à lista de ingressos vendidos
+     * @param ingressosVendidos a lista com os ingressos vendidos
+     * @param usuario o usuario que comprou o ingresso
+     */
+    public void adicionaIngresso(Ingresso ingresso, Usuario usuario){
+        this.ingressosVendidos.add(ingresso); 
+    }
+
+    /**
+     * Retorna a lista com os ingressos vendidos
+     * @return lista ingressos vendidos
+     */
+    public List<Ingresso> getIngressosVendidos(){
+        return this.ingressosVendidos;
+    }
+
+    /**
+     * Calcula o valor total de faturamento
+     * @return o faturamento total
+     */
+    public double calcularFaturamento(){
+        double FaturamentoTotal = 0.0;
+
+        for(Ingresso ingresso : ingressosVendidos){
+            FaturamentoTotal += ingresso.getPreco();
+        }
+
+        return FaturamentoTotal;
     }
 }
