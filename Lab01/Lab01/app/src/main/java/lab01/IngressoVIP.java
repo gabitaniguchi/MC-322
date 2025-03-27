637b78435e85e9d61a763599e5f0f0765689f08d
Gabriela Taniguchi <110126586+gabitaniguchi@users.noreply.github.com>
2025-03-27 12:18:17 -0300

    Update IngressoVIP.java

--- Lab01/Lab01/app/src/main/java/lab01/IngressoVIP.java
+++ Lab01/Lab01/app/src/main/java/lab01/IngressoVIP.java
@@ -1,5 +1,26 @@
 package lab01;
 
 public class IngressoVIP extends Ingresso {
-    //IMPLEMENTAR
+
+    /**
+     * Construtor da classe IngressoVIP
+     * @param evento o evento associado ao IngressoVIP
+     */
+    public IngressoVIP(Evento evento){
+        super(evento);
+    }
+
+    /**
+     * Retorna o preço do Ingresso
+     * @return o preço do Ingresso
+     */
+
+    @Override
+    public double getPreco() {
+        // Ingresso VIP recebe o valor integral do ingresso do evento
+        return this.getEvento().getPrecoIngresso();
+    }
+
+
 }
+
