510697eb3fcce8f03c5e1114eb7099d028648556
gabitaniguchi <andradetaniguchi@gmail.com>
2025-05-20 18:50:06 -0300

    comecin

---
+++ Lab02/app/src/main/java/lab02/Marketplace.java
@@ -0,0 +1,18 @@
+package lab02;
+
+import java.util.List;
+
+public class Marketplace {
+
+    List<Ingresso> ingressossAVenda;
+    double comissaoPorcentagem;
+
+    /*
+     * Retorna a lista de ingressos/ofertas disponiveis no 
+     */
+    List<Ingresso> listarOrfertas(){
+        return ingressossAVenda;
+
+    }
+    
+}
